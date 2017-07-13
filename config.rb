6af8d18310d4c80dbe5081b82a50aa00ceb098a8
Jake Johnson <jake@fastdivision.com>
2017-07-13 12:30:42 -0700

    Ignore asset hashing for client and integration icons

--- config.rb
+++ config.rb
@@ -32,6 +32,6 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
   # activate :relative_assets
-  activate :asset_hash
+  activate :asset_hash, ignore: [/images\/clients/, /images\/integrations/]
   # activate :gzip
 end
