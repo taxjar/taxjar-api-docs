43e7b89f821bfdd00e64e3d87f5d79c6abc0e06b
ScottRudiger <scottrudiger@gmail.com>
2020-07-20 08:36:46 -0700

    Add categories dynamically via DataTables ajax

--- source/javascripts/app/datatable.js
+++ source/javascripts/app/datatable.js
@@ -2,6 +2,23 @@
   'use strict';
 
   var table = $('.datatable').next('table').DataTable({
+    ajax: function(_, callback) {
+      $.ajax({
+        url: 'https://api.taxjar.com/v2/categories',
+        type: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + global.apiToken
+        }
+      }).always(handleErr(callback), addFlags(callback))
+    },
+    deferRender: true,
+    columns: [
+      {data: 'name'},
+      {data: 'product_tax_code'},
+      {data: 'flags'},
+      {data: 'description'}
+    ],
     autoWidth: false,
     info: false,
     paging: true,
@@ -27,5 +44,29 @@
       scrollTop: $('#request').offset().top
     }, 'slow');
   });
+
+  var flagList = {
+    default: '<span class="flag-icon flag-icon-us drop-theme-taxjar-popovers drop-target" data-tooltip="United States" data-tooltip-position="top center"></span>'
+  };
+
+  function addFlags(callback) {
+    return function (res) {
+      callback({
+        data: res.categories.map(function (category) {
+          category.flags = flagList[category.product_tax_code] || flagList.default;
+          return category;
+        })
+      });
+    };
+  };
+
+  function handleErr(callback) {
+    return function (res) {
+      if (res.status != 200) {
+        // TODO: add retry mechanism
+        // TODO: pull in honeybadger
+      }
+    };
+  };
 
 })(window);
