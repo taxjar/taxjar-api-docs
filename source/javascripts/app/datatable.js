d3c43ecae821260239c98e8cfe42aff76aaab682
ScottRudiger <scottrudiger@gmail.com>
2020-07-24 10:29:07 -0700

    Add categories dynamically via DataTables ajax

--- source/javascripts/app/datatable.js
+++ source/javascripts/app/datatable.js
@@ -2,6 +2,22 @@
   'use strict';
 
   var table = $('.datatable').next('table').DataTable({
+    ajax: function (_, callback) {
+      $.ajax({
+        url: 'https://api.taxjar.com/v2/categories',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + global.apiToken
+        }
+      }).done(addFlags(callback));
+    },
+    deferRender: true,
+    columns: [
+      {data: 'name'},
+      {data: 'product_tax_code'},
+      {data: 'flags'},
+      {data: 'description'}
+    ],
     autoWidth: false,
     info: false,
     paging: true,
@@ -21,11 +37,26 @@
     }
   });
 
-  table.on('page.dt', function() {
+  table.on('page.dt', function () {
     $([document.documentElement, document.body]).animate({
       // Note: '#get-list-tax-categories' scrolls too far up on mobile so using '#request'
       scrollTop: $('#request').offset().top
     }, 'slow');
   });
+
+  var flagList = {
+    default: '<span class="flag-icon flag-icon-us drop-theme-taxjar-popovers drop-target" data-tooltip="United States" data-tooltip-position="top center"></span>'
+  };
+
+  function addFlags (callback) {
+    return function (res) {
+      callback({
+        data: res.categories.map(function (category) {
+          category.flags = flagList[category.product_tax_code] || flagList.default;
+          return category;
+        })
+      });
+    };
+  };
 
 })(window);
